bbd31a0323372aa3547b70821ea641ea73c4cf02
Daksh Sharma <dakshsharma0214@gmail.com>
2023-04-14 10:35:47 -0400

    Commmit

--- app/src/main/java/com/cst2335/androidfinalproject/data/model/LogInUser.java
+++ app/src/main/java/com/cst2335/androidfinalproject/data/model/LogInUser.java
@@ -3,25 +3,25 @@ package com.cst2335.androidfinalproject.data.model;
 /**
  * Author:Daksh Sharma
  * Date updated: 8/4/2023
- * purpose:A user who has successfully logged into the programme is represented by the class LogInUser. When a new LogInUser object is created, its two private properties, userId and displayName. A user's userId field serves
+ * purpose:A user who has successfully logged into the programme is represented by the class LoggedInUser. When a new LoggedInUser object is created, its two private properties, userId and displayName. A user's userId field serves
  * as a distinctive identification, while the displayName field serves as a name that may be seen by other users.
  */
 public class LogInUser {
 
-    private String idUser;
+    private String userId;
     private String displayName;
 
 
-    public LogInUser(String idUser, String displayName) {
-        this.idUser = idUser;
+    public LogInUser(String userId, String displayName) {
+        this.userId = userId;
         this.displayName = displayName;
     }
     public String getDisplayName() {
         return displayName;
     }
 
-    public String getIdUser() {
-        return idUser;
+    public String getUserId() {
+        return userId;
     }
 
 
